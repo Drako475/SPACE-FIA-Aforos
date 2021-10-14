5feffea0396d85d78ab9c51726ff4f785e7edbed
Erik Espinoza <erik_espinoza_mendoza@hotmail.com>
2021-10-14 00:27:57 -0500

    Agregado la columna "Pisos" en el crud del admin

--- SPACE FIA/app.js
+++ SPACE FIA/app.js
@@ -19,12 +19,13 @@ var firebaseConfig = {
     let id = $('#id').val();
     let codigo = $('#codigo').val();
     let descripcion = $('#descripcion').val();
-    let cantidad = $('#cantidad').val();
+    let aforoTotal = $('#aforoTotal').val();
+    let aforoActual =$('#aforoActual').val();
     let idFirebase = id;
     if(idFirebase == ''){
      idFirebase = coleccionPisos.push().key;
     };
-    data = {codigo:codigo, descripcion: descripcion, cantidad: cantidad};
+    data = {codigo:codigo, descripcion: descripcion, aforoTotal: aforoTotal, aforoActual: aforoActual};
     actualizacionData = {};
     actualizacionData[`/${idFirebase}`] = data;
     coleccionPisos.update(actualizacionData);
@@ -34,11 +35,12 @@ var firebaseConfig = {
   });
   const iconoEditar = '<svg class="bi bi-pencil-square" width="1em" height="1em" viewBox="0 0 16 16" fill="currentColor" xmlns="http://www.w3.org/2000/svg"><path d="M15.502 1.94a.5.5 0 0 1 0 .706L14.459 3.69l-2-2L13.502.646a.5.5 0 0 1 .707 0l1.293 1.293zm-1.75 2.456l-2-2L4.939 9.21a.5.5 0 0 0-.121.196l-.805 2.414a.25.25 0 0 0 .316.316l2.414-.805a.5.5 0 0 0 .196-.12l6.813-6.814z"/><path fill-rule="evenodd" d="M1 13.5A1.5 1.5 0 0 0 2.5 15h11a1.5 1.5 0 0 0 1.5-1.5v-6a.5.5 0 0 0-1 0v6a.5.5 0 0 1-.5.5h-11a.5.5 0 0 1-.5-.5v-11a.5.5 0 0 1 .5-.5H9a.5.5 0 0 0 0-1H2.5A1.5 1.5 0 0 0 1 2.5v11z"/></svg>';
   const iconoBorrar = '<svg class="bi bi-trash" width="1em" height="1em" viewBox="0 0 16 16" fill="currentColor" xmlns="http://www.w3.org/2000/svg"><path d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0V6z"/><path fill-rule="evenodd" d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1v1zM4.118 4L4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4H4.118zM2.5 3V2h11v1h-11z"/></svg>';
-  function mostrarPisos({codigo, descripcion, cantidad}){
+  function mostrarPisos({codigo, descripcion, aforoTotal, aforoActual}){
     return `
     <td>${codigo}</td>
     <td>${descripcion}</td>
-    <td>${cantidad}</td>
+    <td>${aforoTotal}</td>
+    <td>${aforoActual}</td>
     <td><button class="btnEditar btn btn-secondary" data-toggle="tooltip" title="Editar">${iconoEditar}</button><button class="btnBorrar btn btn-danger" data-toggle="tooltip" title="Borrar">${iconoBorrar}</button></td>
     `
   };
@@ -64,7 +66,8 @@ var firebaseConfig = {
     $('#id').val('');
     $('#codigo').val('');
     $('#descripcion').val('');
-    $('#cantidad').val('');
+    $('#aforoTotal').val('');
+    $('#aforoActual').val('');
     $('form').trigger('reset');
     $('#modalAltaEdicion').modal('show');
   });
@@ -72,11 +75,13 @@ var firebaseConfig = {
     let id = $(this).closest('tr').attr('id');
     let codigo = $(this).closest('tr').find('td:eq(0)').text();
     let descripcion = $(this).closest('tr').find('td:eq(1)').text();
-    let cantidad = $(this).closest('tr').find('td:eq(2)').text();
+    let aforoTotal = $(this).closest('tr').find('td:eq(2)').text();
+    let aforoActual = $(this).closest('tr').find('td:eq(3)').text();
     $('#id').val(id);
     $('#codigo').val(codigo);
     $('#descripcion').val(descripcion);                
-    $('#cantidad').val(cantidad);                
+    $('#aforoTotal').val(aforoTotal);
+    $('#aforoActual').val(aforoActual);                    
     $('#modalAltaEdicion').modal('show');
   });
   $('#tablaPisos').on('click', '.btnBorrar', function(){
